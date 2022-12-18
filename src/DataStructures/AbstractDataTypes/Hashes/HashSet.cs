9bf1492de454f00e62e90ae09c91c681c5d3ddc2
Hamed Nikzad <nikzad1603@gmail.com>
2022-12-18 07:16:58 +0330

    Continue HashSet.cs

--- src/DataStructures/AbstractDataTypes/Hashes/HashSet.cs
+++ src/DataStructures/AbstractDataTypes/Hashes/HashSet.cs
@@ -4,15 +4,17 @@ public class HashSet<T>
 {
     private struct Entry
     {
-        public int hashCode;
+        public int HashCode;
         public int Next;
         public T Value;
     }
     
     private Entry[] _entries;
     private int[]? _buckets;
-    private int _size;
+    private int _count;
     private int _capacity;
+    private int _freeList;
+    private int _freeCount;
 
     public int Count { get; private set; }
     public int Capacity => _capacity;
@@ -41,11 +43,21 @@ public class HashSet<T>
         _entries = new Entry[capacity];
     }
 
-    private static int GetHash(T item)
+    private void Resize()
     {
-        return item!.GetHashCode();
+        var newSize = _capacity * 2;
+        Resize(newSize);
     }
+    private void Resize(int newSize)
+    {
+        var newEntries = new Entry[newSize];
+        var newBuckets = new int[newSize];
+        
+        //Copy
 
+        _capacity = newSize;
+    }
+    
     public bool Add(T item)
     {
         if (item is null)
@@ -55,17 +67,57 @@ public class HashSet<T>
         {
             Initialize(0);
         }
-        
+        var comparer = EqualityComparer<T>.Default;
+
         var hashCode = GetHash(item);
         var bucket = GetBucket(hashCode);
+        while (bucket >= 0)
+        {
+            var entry = _entries[bucket];
+            if (entry.HashCode == hashCode && comparer.Equals(entry.Value, item))
+            {
+                //Item already exists
+                return false;
+            }
+
+            bucket = entry.Next;
+        }
         
+        int index;
+        if (_freeCount > 0)
+        {
+            index = _freeList;
+            _freeCount--;
+            _freeList = StartOfFreeList - entries[_freeList].Next;
+        }
+        else
+        {
+            int count = _count;
+            if (count == _entries.Length)
+            {
+                Resize();
+                bucket = GetBucket(hashCode);
+            }
+            index = count;
+            _count = count + 1;
+        }
+
+        {
+            var entry = _entries![index];
+            entry.HashCode = hashCode;
+            entry.Next = bucket - 1; // Value in _buckets is 1-based
+            entry.Value = item;
+            bucket = index + 1;
+        }
         
-        
-        
-        Count++;
         return true;
     }
-
+    
+    private static int GetHash(T item)
+    {
+        return item!.GetHashCode();
+    }
+    
     private int GetBucket(int hashCode)
     {
         return _buckets![(uint)hashCode % (uint)_buckets.Length];
@@ -73,28 +125,41 @@ public class HashSet<T>
 
     public bool Contains(T item)
     {
-        var comparer = EqualityComparer<T>.Default;
-        var index = GetHash(item);
-
-        var itemAtIndex = _entries[index];
+        if (_buckets is null || item is null)
+            return false;
         
-        return itemAtIndex is not null && comparer.Equals(itemAtIndex, item);
-    }
-
-    public bool Remove(T item)
-    {
         var comparer = EqualityComparer<T>.Default;
-        var index = GetHash(item);
 
-        var itemAtIndex = _entries[index];
-
-        if (itemAtIndex is not null && comparer.Equals(itemAtIndex, item))
+        var hashCode = GetHash(item);
+        var bucket = GetBucket(hashCode);
+        while (bucket >= 0)
         {
-            _entries[index] = default;
-            Count--;
-            return true;
+            var entry = _entries[bucket];
+            if (entry.HashCode == hashCode && comparer.Equals(entry.Value, item))
+            {
+                return true;
+            }
+
+            bucket = entry.Next;
         }
 
         return false;
     }
+
+    // public bool Remove(T item)
+    // {
+    //     var comparer = EqualityComparer<T>.Default;
+    //     var index = GetHash(item);
+    //
+    //     var itemAtIndex = _entries[index];
+    //
+    //     if (itemAtIndex is not null && comparer.Equals(itemAtIndex, item))
+    //     {
+    //         _entries[index] = default;
+    //         Count--;
+    //         return true;
+    //     }
+    //
+    //     return false;
+    // }
 }
